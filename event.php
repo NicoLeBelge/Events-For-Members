356b886e06a86dad0d15adfc1146f6b72556873c
NicoLeBelge <nicolas@lamblain.fr>
2021-04-18 21:10:43 +0200

    css changes (trn --> E4M)

--- event.php
+++ event.php
@@ -9,7 +9,7 @@ session_start();
 		<meta name="description" content="Inscrivez-vous en ligne aux tournois annoncés !">
 		<meta name="keywords" content="tournois, inscriptions">
         <link rel="stylesheet" href="../css/ChessMOOC-style.css" /> 
-		<link rel="stylesheet" href="./css/trn.css" /> 
+		<link rel="stylesheet" href="./css/E4M.css" /> 
 		<link rel="icon" type="image/png" href="../img/logo-3-96.png" />
         <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet"> 
     </head>
