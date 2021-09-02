9466c79d9def83b4093ee3ed2336d50fc1207032
NicoLeBelge <nicolas@lamblain.fr>
2021-09-02 22:55:35 +0200

    formulaire création event - fin de refonte

--- core/edit-create-event-core.php
+++ core/edit-create-event-core.php
@@ -17,25 +17,33 @@ $jsonstr = json_encode($str);
     <h2><?=$str["event_creation_title"]?></h2>
     <form action="./core/createEvent-Action-core.php" method="post">
     
-            <label for="name"><?=$str["event_name_label"]?></label>  <input type="text" id="name" name="name"required/>
-            <p>Date de début de l'évènement : <input type="date" name="datestart" required/></p>
-            <p>Date de fin de l'évènement : <input type="date" name="datelim" /></p>
-            <p>Sécutisation de l'évènement :
+            <label for="name"><?=$str["event_name_label"]?></label><span style="color: red"> *</span>
+            <input type="text" id="name" name="name" required/>
+            <label for="datestart"><?=$str["Date_of_place"]?></label>  
+            <input type="date" name="datestart" required/><span style="color: red"> *</span>
+            <label for="datelim"><?=$str["Date_until"]?></label>  
+            <input type="date" name="datelim" />
+
+            <p><?=$str["Event_secured_info"]?>
                 <input type="radio" id="yes" name="secured" value="yes" checked>
                 <label for="yes">yes</label>
 
                 <input type="radio" id="no" name="secured" value="no">
                 <label for="no">no</label>
             </p> 
-                <label for="mail">e-mail de contact :</label>
+                <label for="mail"><?=$str["Organizer_email"]?></label><span style="color: red"> *</span>
                 <input type="email" id="mail" name="contact" required/>
+                
+            <label for="nbmax"><?=$str["Nb_max_participants"]?></label>   
+            <input type="number" name="nbmax" />
+            <br/>
+            
+            <label for="pos_lat"><?=$str["geoloc_lat_long"]?></label>   
+            <input type="number" step="any" name="pos_lat" />
+            <input type="number" step="any" name="pos_long" />
+            
 
-            <p>Nombre maximal d'inscrits : <input type="number" name="nbmax" required /></p>
-
-            <p>Position longitude : <input type="number" step="any" name="pos_long" /></p>
-            <p>Position latitude : <input type="number" step="any" name="pos_lat" /></p>
-
-            <p><input type="submit" value="OK" id="submitButton"></p>
+            <p><input type="submit" value="<?=$str["Validate"]?>" id="submitButton"></p>
             <input id="id" name="id" type="hidden" value=<?php echo $_SESSION['user_id'] ?>>
     </form>	
 	
