a9be3444d93bf1eea5c7fae255baf898bebcd1db
DiveGuillaume <guillaume53.dive@gmail.com>
2021-09-12 11:46:19 +0200

    Ajout de la classe CSS

--- core/editEvent-core.php
+++ core/editEvent-core.php
@@ -8,9 +8,7 @@
 	{
 	// echo('<br />'.modifAuthorization($conn)['message'].'<br />');
 ?>
-<div style="border: 4mm ridge grey; padding: 1em;">
 	<?php include('./core/editEvent-formulaire-core.php'); ?>
-</div>
 <script type="text/javascript">
 	validate();
 </script> 
--- register-event-edition.php
+++ register-event-edition.php
@@ -10,17 +10,28 @@
 		<meta name="description" content="Editer un event">
 		<meta name="keywords" content="tournois, inscription, modification">
 
+        <link rel="stylesheet" href="../css/ChessMOOC-style.css" /> 
 		<link rel="stylesheet" href="./css/E4M.css" /> 
         <link rel="preconnect" href="https://fonts.gstatic.com">
 		<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
-		<script type="text/javascript" src="./JS/verifDataForm.js"></script>  
     </head>
 
     <body >
 
-    <?php
+    <?php 
+	
+        include ('../head-foot/T0P-menu.php');
+        
+        echo "<div class='cent_block'> \n";
+        echo "<br/> \n";
         $path = './core/editEvent-core.php';
         if(!empty($_GET['id'])) include($path);
-    ?>
+    
+        echo "</div> \n";
+	
+        echo "<div class='T0P-footer'> \n";
+        include ('../head-foot/T0P-footer.php');
+        echo "</div> \n";
+        ?>
     </body>
 </html>
