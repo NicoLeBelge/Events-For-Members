3e898e7cb34f50497a3a4760f93693e164f068cf
NicoLeBelge <nicolas@lamblain.fr>
2023-07-24 14:06:46 +0200

    member edit files added - to be completed

---
+++ edit-member-action.php
@@ -0,0 +1,21 @@
+
+
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Event modification</title>
+		<meta charset="UTF-8" />
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<link rel="stylesheet" href="./_css/E4M.css" /> 
+    </head>
+
+    <body >
+    <?php 
+        $path = './core/edit-member-action-core.php';
+        include($path);
+    ?>
+    </body>
+</html>
