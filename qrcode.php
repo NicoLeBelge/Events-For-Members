857bdab1c17339045f4062d2a867344e76caa156
NicoLeBelge <nicolas@lamblain.fr>
2024-05-07 13:55:50 +0200

    qrcode page added

---
+++ qrcode.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>check-in</title>
+		<meta charset="UTF-8" />
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+		<link rel="stylesheet" href="./_css/E4M.css" /> 
+    </head>
+
+    <body >
+    <?php 
+        $path = './core/qrcode-core.php';
+        include($path);
+    ?>
+    </body>
+</html>
